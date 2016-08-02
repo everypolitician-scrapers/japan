62a9fa2b4a3c56254f873be7b62023a15a8b5cf0
Struan Donald <struan@exo.org.uk>
2016-08-02 14:53:29 +0100

    update to use page archiver

--- scraper.rb
+++ scraper.rb
@@ -3,12 +3,13 @@
 
 require 'scraperwiki'
 require 'nokogiri'
-require 'open-uri/cached'
-
-OpenURI::Cache.cache_path = '.cache'
+require 'scraped_page_archive'
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read)
+  ScrapedPageArchive.record do
+    response = open(url.to_s).read
+    Nokogiri::HTML(response)
+  end
 end
 
 def get_gender(name)
