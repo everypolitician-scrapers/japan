9e96938ca542bbf7f26e65ce1b82662eebcf1e13
Tony Bowden <tony@tmtm.com>
2018-09-18 11:21:02 +0100

    use more communicative parameter name

--- scraper.rb
+++ scraper.rb
@@ -12,8 +12,8 @@ require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 # require 'scraped_page_archive/open-uri'
 
-def scrape(h)
-  url, klass = h.to_a.first
+def scrape(pair)
+  url, klass = pair.to_a.first
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
